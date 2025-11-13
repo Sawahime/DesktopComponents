73d0e83b395d80a8b3ebd310192a7366ab895624
louis <805846734@qq.com>
2025-11-13 18:00:02 +0800

    feat: layout log window

--- Redmine/Redmine/logger.cpp
+++ Redmine/Redmine/logger.cpp
@@ -15,16 +15,13 @@ void Logger::CreateLogWindow(HWND hParent) {
 	wc.lpszClassName = L"LogWindowClass";
 	RegisterClassExW(&wc);
 
-	int width = 300;
-
-	// 获取父窗口位置和尺寸
 	RECT parentRect;
 	GetWindowRect(hParent, &parentRect);
 
-	// 计算日志窗口位置（紧挨着主窗口左侧）
-	int x = parentRect.left - width;
-	int y = parentRect.top;
+	int width = 600;
 	int height = parentRect.bottom - parentRect.top;
+	int x = parentRect.left - width + 15;
+	int y = parentRect.top;
 
 	m_hWnd = CreateWindowW(
 		L"LogWindowClass", L"日志窗口",
@@ -32,6 +29,91 @@ void Logger::CreateLogWindow(HWND hParent) {
 		x, y, width, height,
 		hParent, nullptr, GetModuleHandle(NULL), nullptr
 	);
+	if (!m_hWnd) return;
+
+	RECT clientRect;
+	GetClientRect(m_hWnd, &clientRect);
+	int clientWidth = clientRect.right - clientRect.left;
+	int clientHeight = clientRect.bottom - clientRect.top;
+
+	int spacing = 10;// 纵向区域间隔
+	int startY = 0;// It will increase as the number of controls increases.
+
+#pragma region TextArea
+	int textBoxHeight = clientHeight * 9 / 10;
+	m_hEditLog = CreateWindowW(
+		L"EDIT", L"",
+		WS_VISIBLE | WS_CHILD | WS_VSCROLL | ES_LEFT | ES_MULTILINE | ES_AUTOVSCROLL | ES_READONLY,
+		0, startY, clientWidth - 10, textBoxHeight,
+		m_hWnd, (HMENU)IDC_EDIT_LOG, GetModuleHandle(NULL), nullptr
+	);
+	// 设置字体
+	HFONT hFont = CreateFontW(
+		16, 0, 0, 0, FW_NORMAL, FALSE, FALSE, FALSE,
+		DEFAULT_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS,
+		DEFAULT_QUALITY, DEFAULT_PITCH | FF_DONTCARE, L"Consolas"
+	);
+	if (hFont) {
+		SendMessage(m_hEditLog, WM_SETFONT, (WPARAM)hFont, TRUE);
+		SendMessage(m_hEditLog, WM_SETTEXT, 0, (LPARAM)L"text demo");
+	}
+	startY += textBoxHeight + spacing;
+#pragma endregion
+
+#pragma region SplitterArea
+	int splitterHeight = 2;
+	m_hStaticSplitter = CreateWindowW(
+		L"STATIC", L"",
+		WS_VISIBLE | WS_CHILD | SS_ETCHEDHORZ,
+		5, startY, clientWidth - 10, splitterHeight,
+		m_hWnd, (HMENU)IDC_STATIC_SPLITTER, GetModuleHandle(NULL), nullptr
+	);
+	startY += splitterHeight + spacing;
+#pragma endregion
+
+#pragma region ButtonArea
+	int buttonWidth = 100;
+	int buttonHeight = clientHeight - startY - spacing;
+	int buttonSpacing = 20;
+
+	int totalButtonsWidth = 3 * buttonWidth + 2 * buttonSpacing;
+	int startX = (clientWidth - totalButtonsWidth) / 2;
+
+	m_hBtnClear = CreateWindowW(
+		L"BUTTON", L"Clear",
+		WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON,
+		startX, startY, buttonWidth, buttonHeight,
+		m_hWnd, (HMENU)IDC_BTN_CLEAR, GetModuleHandle(NULL), nullptr
+	);
+	startX += buttonWidth + buttonSpacing;
+
+	m_hBtnCopy = CreateWindowW(
+		L"BUTTON", L"Copy",
+		WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON,
+		startX, startY, buttonWidth, buttonHeight,
+		m_hWnd, (HMENU)IDC_BTN_COPY, GetModuleHandle(NULL), nullptr
+	);
+	startX += buttonWidth + buttonSpacing;
+
+	m_hBtnSave = CreateWindowW(
+		L"BUTTON", L"Save",
+		WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON,
+		startX, startY, buttonWidth, buttonHeight,
+		m_hWnd, (HMENU)IDC_BTN_SAVE, GetModuleHandle(NULL), nullptr
+	);
+
+	// 设置按钮字体
+	HFONT hButtonFont = CreateFontW(
+		24, 0, 0, 0, FW_NORMAL, FALSE, FALSE, FALSE,
+		DEFAULT_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS,
+		DEFAULT_QUALITY, DEFAULT_PITCH | FF_DONTCARE, L"微软雅黑"
+	);
+	if (hButtonFont) {
+		SendMessage(m_hBtnClear, WM_SETFONT, (WPARAM)hButtonFont, TRUE);
+		SendMessage(m_hBtnCopy, WM_SETFONT, (WPARAM)hButtonFont, TRUE);
+		SendMessage(m_hBtnSave, WM_SETFONT, (WPARAM)hButtonFont, TRUE);
+	}
+#pragma endregion
 }
 
 
--- Redmine/Redmine/logger.h
+++ Redmine/Redmine/logger.h
@@ -10,6 +10,13 @@
 #define FunctionEntryLog DebugPrint(L" Entry" << std::endl);
 #define FunctionExitLog DebugPrint(L" Exit" << std::endl);
 
+// 控件ID定义
+#define IDC_EDIT_LOG          1001
+#define IDC_STATIC_SPLITTER   1002
+#define IDC_BTN_CLEAR         1003
+#define IDC_BTN_COPY          1004
+#define IDC_BTN_SAVE          1005
+
 class Logger {
 public:
 	void CreateLogWindow(HWND hParent);
@@ -18,5 +25,10 @@ private:
 	static LRESULT CALLBACK LogWndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
 
 private:
-	HWND m_hWnd;
+	HWND m_hWnd;           // 主窗口句柄
+	HWND m_hEditLog;       // 日志文本框句柄
+	HWND m_hStaticSplitter;// 分割线句柄
+	HWND m_hBtnClear;      // 清空按钮句柄
+	HWND m_hBtnCopy;       // 复制按钮句柄
+	HWND m_hBtnSave;       // 保存按钮句柄
 };
